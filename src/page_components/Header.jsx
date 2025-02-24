4b5e75b3ca82b0694e50ec9d14af7ae93c62aaaa
penso996 <penso.andrea@outlook.it>
2025-02-24 15:30:31 +0100

    working on navbar

--- src/page_components/Header.jsx
+++ src/page_components/Header.jsx
@@ -1,9 +1,18 @@
 // Header.jsx
 
+// Import Navlink functions
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
     return (
         <header>
             <h1>BLOG</h1>
+            <div>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/blog_posts"}>Blog Posts</NavLink>
+                <NavLink to={"/about_us"}>About Us</NavLink>
+            </div>
+
         </header>
     );
 }
