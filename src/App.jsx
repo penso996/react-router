7668f91ed4ebfed5f663a71ec24b87fe53f68667
penso996 <penso.andrea@outlook.it>
2025-02-24 14:52:56 +0100

    working on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import Header from "./page_components/Header"
 
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/page_components/Header.jsx
@@ -0,0 +1,9 @@
+// Header.jsx
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Blog</h1>
+        </header>
+    );
+}
