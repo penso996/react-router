da7d5aa8978dafd7b2aef993d80e10881dc70b26
penso996 <penso.andrea@outlook.it>
2025-02-25 15:04:07 +0100

    added post detail page

--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -1,11 +1,39 @@
 // PostDetailPage.jsx
 
+// Import useState from React
+import { useState, useEffect } from 'react';
+// Import useParams for routing
+import { useParams } from "react-router-dom";
+// Import axios
+import axios from 'axios';
 
-// RENDER
 export default function PostDetailPage() {
+
+    // get the post ID from the URL params
+    const { id } = useParams();
+    // useState to set current post from param
+    const [post, setPost] = useState({});
+
+    // FUNCTION to fetch an ID post
+    function fetchPost() {
+        axios.get(`http://localhost:3000/posts/${id}`)
+            .then(res => setPost(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => fetchPost(), [id])
+
+
+    // RENDER
     return (
         <main>
-            <h1>POST DETAIL PAGE</h1>
+            {/* Blog Post*/}
+            <div className="post-container">
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+                <img src={post.image} alt={post.title} />
+                <p>{post.tags?.length === 1 ? post.tags[0] : post.tags?.join(", ")}</p>
+            </div>
         </main>
     );
 }
